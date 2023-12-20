9217f7df5d33eb61887821ad6d4400ec5a1c9853
Baha Khmeissi <bahaeddkh@gmail.com>
2023-12-20 01:39:19 +0100

    worked on personal life section

--- src/components/Life.jsx
+++ src/components/Life.jsx
@@ -34,69 +34,75 @@ const Life = () => {
         <div className="max-w-[1000px]  w-full grid sm:grid-cols-2 gap-8 px-4 ">
           <div data-aos="fade-up" className="sm:text-right text-4xl font-bold">
             <p className="pt-3">
-              <img style={{ height: "300px" }} src={little} alt="" />
+              <img
+                style={{ width: "280px", height: "300px" }}
+                src={little}
+                alt=""
+                className="imgLife"
+              />
             </p>
           </div>
           <div data-aos="fade-right" className="leading-loose sm:my-[20px]">
-            I am passionate Life building excellent software that improves the
-            lives of those around me. I specialize in creating software for
-            clients ranging from individuals and small-businesses all the way to
-            large enterprise corporations. I'm passionate Life bringing both the
-            technical and visual aspects of digital products to life. User
-            experience, beautiful pixels and writing clean accessible, human
-            code matters to me. I sweat the details. What would you do if you
-            had a software expert available at your fingertips?
+            In my childhood, laughter echoed in every corner, as days were a
+            symphony of playful escapades and bedtime tales. Love enveloped me
+            in a comforting embrace, creating a canvas of cherished memories
+            that still paint my world with joy.
           </div>
         </div>
         <div className="max-w-[1000px]  w-full grid sm:grid-cols-2 gap-8 px-4 ">
           <div data-aos="fade-up" className="sm:text-right text-4xl font-bold">
             <p className="pt-3">
-              <img style={{ height: "300px" }} src={gym} alt="" />
+              <img
+                style={{ width: "280px", height: "300px" }}
+                src={gym}
+                alt=""
+                className="imgLife"
+              />
             </p>
           </div>
           <div data-aos="fade-right" className="leading-loose sm:my-[20px]">
-            I am passionate Life building excellent software that improves the
-            lives of those around me. I specialize in creating software for
-            clients ranging from individuals and small-businesses all the way to
-            large enterprise corporations. I'm passionate Life bringing both the
-            technical and visual aspects of digital products to life. User
-            experience, beautiful pixels and writing clean accessible, human
-            code matters to me. I sweat the details. What would you do if you
-            had a software expert available at your fingertips?
+            As I entered adulthood, the gym became my sanctuary, sculpting not
+            just a physique but resilience. Weightlifting transformed from a
+            habit into a passion, fostering discipline and a robust, fit body,
+            intertwining strength with a newfound sense of self.
           </div>
         </div>
         <div className="max-w-[1000px]  w-full grid sm:grid-cols-2 gap-8 px-4 ">
           <div data-aos="fade-up" className="sm:text-right text-4xl font-bold">
             <p className="pt-3">
-              <img style={{ height: "300px" }} src={camp} alt="" />
+              <img
+                style={{ width: "280px", height: "300px" }}
+                src={camp}
+                alt=""
+                className="imgLife"
+              />
             </p>
           </div>
           <div data-aos="fade-right" className="leading-loose sm:my-[20px]">
-            I am passionate Life building excellent software that improves the
-            lives of those around me. I specialize in creating software for
-            clients ranging from individuals and small-businesses all the way to
-            large enterprise corporations. I'm passionate Life bringing both the
-            technical and visual aspects of digital products to life. User
-            experience, beautiful pixels and writing clean accessible, human
-            code matters to me. I sweat the details. What would you do if you
-            had a software expert available at your fingertips?
+            Embracing the allure of camping, my passion evolved into a deep
+            connection with nature. Amidst serene landscapes and crackling
+            campfires, I found solace, adventure, and a pure escape, nurturing
+            my spirit and fostering a profound appreciation for the great
+            outdoors.
           </div>
         </div>
         <div className="max-w-[1000px]  w-full grid sm:grid-cols-2 gap-8 px-4 ">
           <div data-aos="fade-up" className="sm:text-right text-4xl font-bold">
             <p className="pt-3">
-              <img style={{ height: "300px" }} src={code} alt="" />
+              <img
+                style={{ width: "280px", height: "300px" }}
+                src={code}
+                alt=""
+                className="imgLife"
+              />
             </p>
           </div>
           <div data-aos="fade-right" className="leading-loose sm:my-[20px]">
-            I am passionate Life building excellent software that improves the
-            lives of those around me. I specialize in creating software for
-            clients ranging from individuals and small-businesses all the way to
-            large enterprise corporations. I'm passionate Life bringing both the
-            technical and visual aspects of digital products to life. User
-            experience, beautiful pixels and writing clean accessible, human
-            code matters to me. I sweat the details. What would you do if you
-            had a software expert available at your fingertips?
+            Embarking on the journey of learning to code, I immersed myself in
+            the captivating realm of web development. Through countless lines of
+            code, challenges turned into triumphs, leading to the creation of
+            cool websites that reflected both my evolving skills and creative
+            aspirations.
           </div>
         </div>
       </div>
